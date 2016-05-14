371ade78c00e06a4b64a8a7bfb249cc89933847a
Erin Keeffe <edkeeffe@gmail.com>
2016-05-14 15:44:17 +0800

    adjust hero image fade settings

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -26,8 +26,8 @@ $(document).ready(function() {
     var heroScroll = new ScrollMagic.Controller();
 
     // hero image fade
-    var heroFadeTween = TweenMax.to(heroImage, 1, {opacity: 0.04, ease: 'ease'});
-    var heroFade = new ScrollMagic.Scene({triggerElement: "#content", triggerHook: 'onEnter', offset: 10, duration: 900})
+    var heroFadeTween = TweenMax.to(heroImage, 1, {opacity: 0, ease: 'ease'});
+    var heroFade = new ScrollMagic.Scene({triggerElement: "#content", triggerHook: 'onEnter', duration: 800})
                         .setTween(heroFadeTween)
                         // trigger a velocity opaticy animation
                         // .setVelocity("#animate", {opacity: 0}, {duration: 400})
